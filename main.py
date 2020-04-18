97ffe28650629de12fd44c534f6b0dbc107b03f0
Priyesh Prasad <prasadpriyesh1@gmail.com>
2020-04-19 04:30:16 +0530

    v 1.0

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n\r\n')
 @app.route('/video1')
-def video_feed():
+def video1():
     
     return Response(gen(Camera1()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
